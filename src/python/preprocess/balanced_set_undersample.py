c0da801059f20bdfa3f3004dd68558b040d182c0
Manab Chetia <manab.chetia@outlook.com>
2015-08-25 16:02:09 +1000

    Print line added

--- src/python/preprocess/balanced_set_undersample.py
+++ src/python/preprocess/balanced_set_undersample.py
@@ -13,6 +13,7 @@ import numpy as np
 
 
 
+
 # MINC_DIR = '../../../data/MINC/'
 # CATEGORIES_FILE = join(MINC_DIR, 'minc', 'categories.txt')
 # PATCH_DIR = join(MINC_DIR, 'patch')
@@ -111,5 +112,6 @@ def run_copy(group):
 if __name__ == '__main__':
     create_patch_class_dirs()
     # run_copy('validate')
+    print("Creating balanced trainset")
     run_copy('train')
     # run_copy('test')
